b20e992a9fb9cba78e917681c07cee3ec0346bc6
Daniel Lenz <mail@daniellenz.org>
2018-04-24 12:21:45 -0700

    Add the best-fit parameters from Bowman+ 18

---
+++ edges/this_project.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Best-fit parameters from Bowman+ (2018), taken from the triangle plot
+# (Figure 10)
+B18_BESTFIT = dict(
+    a0=1570.,
+    a1=700.,
+    a2=-1200.,
+    a3=750.,
+    a4=-175.,
+    amp=0.500,  # in K
+    nu0=78.3,   # in MHz
+    w=20.5,     # in MHz
+    tau=6.,
+)
